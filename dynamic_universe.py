daaf415cdee60639762600ede32ed7109e02a46a
ultrametatron <mayankp@live.in>
2025-04-25 15:54:09 +1000

    Update dynamic_universe.py to 20000

--- dynamic_universe.py
+++ dynamic_universe.py
@@ -3,6 +3,6 @@ Generate the global ticker universe for main branch.
 """
 import pandas as pd
 
-def dynamic_universe_all(n=5000):
+def dynamic_universe_all(n=20000):
     # Read the master list and return top-n symbols
     return pd.read_csv("state/master_tickers.csv").Symbol.head(n).tolist()
